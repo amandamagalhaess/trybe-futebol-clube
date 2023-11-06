524059ce917368746f1e0ac0a19accd15a2afca3
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-11-06 18:25:40 -0300

    19 - test coverage TDD

--- app/backend/src/tests/Matches.test.ts
+++ app/backend/src/tests/Matches.test.ts
@@ -2,17 +2,20 @@ import * as sinon from 'sinon';
 import * as chai from 'chai';
 import { App } from '../app';
 import SequelizeMatch from '../database/models/SequelizeMatch';
-import { matches, match } from './mocks/Matches.mock';
+import SequelizeUser from '../database/models/SequelizeUser';
+import { matches, match, newMatch, matchRequest, matchRequestWithEqualTeams } from './mocks/Matches.mock';
+import { user, validLoginBody } from './mocks/Users.mock';
 
 // @ts-ignore
 import chaiHttp = require('chai-http');
+import SequelizeTeam from '../database/models/SequelizeTeam';
 
 chai.use(chaiHttp);
 
 const { app } = new App();
 const { expect } = chai;
 
-describe('Matches Test', () => {
+describe('Matches Test', async () => {
   it('should return all matches', async () => {
     sinon.stub(SequelizeMatch, 'findAll').resolves(matches as any);
 
@@ -20,5 +23,86 @@ describe('Matches Test', () => {
 
     expect(status).to.equal(200);
     expect(body).to.deep.equal(matches);
+  });
+
+  it('should return filtered matches', async () => {
+    sinon.stub(SequelizeMatch, 'findAll').resolves(matches as any);
+
+    const { status, body } = await chai.request(app).get('/matches?inProgress=true');
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal(matches);
+  });
+
+  it('should finish match', async () => {
+    sinon.stub(SequelizeMatch, 'findOne').resolves(match as any);
+    sinon.stub(SequelizeMatch, 'update').resolves([1]);
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).patch('/matches/1/finish').set('authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal({ message: 'Finished' });
+  });
+
+  it('should not finish match', async () => {
+    sinon.stub(SequelizeMatch, 'findOne').resolves(null);
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).patch('/matches/1/finish').set('authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(404);
+    expect(body).to.deep.equal({ message: 'Match not found' });
+  });
+
+  it('should update match', async () => {
+    sinon.stub(SequelizeMatch, 'findOne').resolves(match as any);
+    sinon.stub(SequelizeMatch, 'update').resolves([1]);
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).patch('/matches/1').send({ homeTeamScore: 1, awayTeamScore: 2 })
+    .set('Authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(200);
+    expect(body).to.deep.equal({ message: 'Updated' });
+  });
+
+  it('should create a match', async () => {
+    sinon.stub(SequelizeMatch, 'create').resolves(newMatch as any);
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).post('/matches').send(matchRequest).set('Authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(201);
+    expect(body).to.deep.equal(newMatch);
+  });
+
+  it('should not create a match with two equal teams', async () => {
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).post('/matches').send(matchRequestWithEqualTeams).set('Authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(422);
+    expect(body).to.deep.equal({ message: 'It is not possible to create a match with two equal teams' });
+  });
+
+  it('should not create a match with invalid teams', async () => {
+    sinon.stub(SequelizeTeam, 'findOne').resolves(null);
+    sinon.stub(SequelizeUser, 'findOne').resolves(user as any);
+    const { body: {token} } = await chai.request(app).post('/login').send(validLoginBody);
+
+    const { status, body } = await chai.request(app).post('/matches').send(matchRequest).set('Authorization', `Bearer ${token}`);
+
+    expect(status).to.equal(404);
+    expect(body).to.deep.equal({ message: 'There is no team with such id!' });
+  });
+
+  afterEach(() => {
+    sinon.restore();
   });
 });
--- app/backend/src/tests/mocks/Matches.mock.ts
+++ app/backend/src/tests/mocks/Matches.mock.ts
@@ -6,13 +6,36 @@ const match = {
   awayTeamGoals: 1,
   inProgress: false,
   homeTeam: {
-    "teamName": "São Paulo"
+    teamName: "São Paulo"
   },
-  "awayTeam": {
-    "teamName": "Grêmio"
+  awayTeam: {
+    teamName: "Grêmio"
   }
 }
 
 const matches = [match];
 
-export { match, matches };
+const newMatch = {
+  id: 1,
+  homeTeamId: 16,
+  homeTeamGoals: 2,
+  awayTeamId: 8,
+  awayTeamGoals: 2,
+  inProgress: true
+}
+
+const matchRequest = {
+  homeTeamId: 16,
+  awayTeamId: 8,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+}
+
+const matchRequestWithEqualTeams = {
+  homeTeamId: 16,
+  awayTeamId: 16,
+  homeTeamGoals: 2,
+  awayTeamGoals: 2
+}
+
+export { match, matches, newMatch, matchRequest, matchRequestWithEqualTeams };
