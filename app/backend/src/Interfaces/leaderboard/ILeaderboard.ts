1227f786749ed1210df61cfb1c094fe5313b4f91
Amanda Magalhaes <mandaamagalhaes@gmail.com>
2023-11-07 01:30:42 -0300

    24 e 25 - update endpoint GET /leaderboard/home

--- app/backend/src/Interfaces/leaderboard/ILeaderboard.ts
+++ app/backend/src/Interfaces/leaderboard/ILeaderboard.ts
@@ -1,10 +1,10 @@
-export type ILeaderboard = {
-  name: string;
-  totalPoints: number;
-  totalGames: number;
-  totalVictories: number;
-  totalDraws: number;
-  totalLosses: number;
-  goalsFavor: number;
-  goalsOwn: number;
-};
+export interface ILeaderboard {
+  name: string,
+  totalPoints: number,
+  totalGames: number,
+  totalVictories: number,
+  totalDraws: number,
+  totalLosses: number,
+  goalsFavor: number,
+  goalsOwn: number,
+}
--- app/backend/src/services/LeaderboardService.ts
+++ app/backend/src/services/LeaderboardService.ts
@@ -8,6 +8,24 @@ export default class LeaderboardService {
   async getLeaderboardHome(): Promise<ServiceResponse<ILeaderboard[]>> {
     const leaderboard = await this.leaderboardModel.getLeaderboardHome();
 
-    return { status: 'SUCCESSFUL', data: leaderboard };
+    const fullLeaderboard = leaderboard
+      .map((item) => (
+        { ...item,
+          goalsBalance: item.goalsFavor - item.goalsOwn,
+          efficiency: ((item.totalPoints / (item.totalGames * 3)) * 100).toFixed(2) }));
+
+    const sortedLeaderboard = fullLeaderboard.sort((a, b) => {
+      if (a.totalPoints > b.totalPoints) return -1;
+      if (a.totalPoints < b.totalPoints) return 1;
+      if (a.totalVictories > b.totalVictories) return -1;
+      if (a.totalVictories < b.totalVictories) return 1;
+      if (a.goalsBalance > b.goalsBalance) return -1;
+      if (a.goalsBalance < b.goalsBalance) return 1;
+      if (a.goalsFavor > b.goalsFavor) return -1;
+      if (a.goalsFavor < b.goalsFavor) return 1;
+      return 0;
+    });
+
+    return { status: 'SUCCESSFUL', data: sortedLeaderboard };
   }
 }
